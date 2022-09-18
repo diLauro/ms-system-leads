0ad387016512f387e56b6820da0c557b79b335af
Eduardo Di Lauro <elauro@magnasistemas.com.br>
2022-09-18 18:30:04 -0300

    ajuste start

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,3 @@
-// import express from "express";
-// import { router } from "./routes";
-// import cors from "cors";
-// import { db } from "./database/db";
+import App from "./App";
 
-
-
-import app from "./app";
-
-// const app = express();
-// app.use(cors());
-// app.use(json());
-// app.use(router);
-
-// app.listen(3000, async () => {
-//   await db.sync();
-//   console.log(`App running on 3000`);
-// });
-
-app.listen(8080);
+App.listen(8080);
