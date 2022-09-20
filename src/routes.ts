78ca6bbd92875a8ea4f579b4f732ec69214cc0db
Eduardo Di Lauro <elauro@magnasistemas.com.br>
2022-09-19 21:01:22 -0300

    feat: midias

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import DomainController from "./controllers/DomainController";
+import MidiaController from "./controllers/MidiaController";
 import UseController from "./controllers/UserController";
 
 const routes = express.Router();
@@ -9,6 +10,12 @@ routes.get("/users", UseController.findAll);
 routes.get("/users/:userId", UseController.findOne);
 routes.put("/users/:userId", UseController.update);
 routes.delete("/users/:userId", UseController.destroy);
+
+routes.post("/midias", MidiaController.create);
+routes.get("/midias", MidiaController.findAll);
+routes.get("/midias/:midiaId", MidiaController.findOne);
+routes.put("/midias/:midiaId", MidiaController.update);
+routes.delete("/midias/:midiaId", MidiaController.destroy);
 
 routes.post("/domains", DomainController.create);
 routes.get("/domains", DomainController.findAll);
